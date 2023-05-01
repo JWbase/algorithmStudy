96497d6c08b2676e0b0ea02b12b5a96ad6648aa9
JWbase <87960006+JWbase@users.noreply.github.com>
2023-05-01 15:30:09 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,8 +1,5 @@
 import math
 def solution(n, m):
-    answer = []
     gcd_nums = math.gcd(n, m)
     lcm_nums = n * m / gcd_nums
-    answer.append(gcd_nums)
-    answer.append(lcm_nums)
-    return answer
+    return [gcd_nums, lcm_nums]
