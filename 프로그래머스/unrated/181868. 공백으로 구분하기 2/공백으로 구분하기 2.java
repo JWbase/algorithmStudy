66867bb551da3756935bef8ff0445b8efb33d987
Archon0426 <87960006+JWbase@users.noreply.github.com>
2023-10-19 12:18:48 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.27 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] tokens = my_string.trim().split("\\s+");
+        return tokens;
+    }
+}
