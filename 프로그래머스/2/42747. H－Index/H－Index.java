77f50a238c89035f3da20159c1001787b86315ad
JWbase <87960006+JWbase@users.noreply.github.com>
2023-11-03 10:45:03 +0900

    [level 2] Title: H-Index, Time: 0.44 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,10 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        int length = citations.length;
-        for (int i = 0; i < length; i++) {
-            if (citations[i] >= length - i) {
-                return length - i;
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length - i) {
+                return citations.length - i;
             }
         }
         return 0;
