a6d5afb483aef40d21d656dc357b2a98101eb3eb
JWbase <87960006+JWbase@users.noreply.github.com>
2023-11-13 09:53:45 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 204 ms, Memory: 21280 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -1,69 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static final int[] dr = {-1, 0, 1, 0};
-    static final int[] dc = {0, 1, 0, -1};
+    static int rowSize, colSize, count;
+    static char[][] map;
+    static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        int testCases = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 0; tc < T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            int H = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
-            boolean[][] map = new boolean[H][W];
-            boolean[][] visit = new boolean[H][W];
+        for (int tc = 0; tc < testCases; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            rowSize = Integer.parseInt(st.nextToken());
+            colSize = Integer.parseInt(st.nextToken());
+            map = new char[rowSize][colSize];
 
-            for (int i = 0; i < H; i++) {
-                String input = br.readLine();
-                for (int j = 0; j < W; j++) {
-                    map[i][j] = input.charAt(j) == '#';
-                }
+            for (int i = 0; i < rowSize; i++) {
+                map[i] = br.readLine().toCharArray();
             }
 
-            sb.append(bfs(H, W, map, visit)).append("\n");
-        }
-
-        sb.setLength(sb.length() - 1);
-        System.out.println(sb);
-    }
-
-    private static int bfs(int H, int W, boolean[][] map, boolean[][] visit) {
-        int result = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                if (map[i][j] && !visit[i][j]) {
-                    result++;
-                    queue.offer(new int[]{i, j});
-                    visit[i][j] = true;
-
-                    while (!queue.isEmpty()) {
-                        int[] cur = queue.poll();
-
-                        for (int d = 0; d < 4; d++) {
-                            int nr = cur[0] + dr[d];
-                            int nc = cur[1] + dc[d];
-
-                            if (nr < 0 || nr >= H || nc < 0 || nc >= W || !map[nr][nc] || visit[nr][nc]) continue;
-                            visit[nr][nc] = true;
-                            queue.offer(new int[]{nr, nc});
-                        }
+            count = 0;
+            for (int i = 0; i < rowSize; i++) {
+                for (int j = 0; j < colSize; j++) {
+                    if (map[i][j] == '#') {
+                        dfs(i, j);
+                        count++;
                     }
                 }
             }
+            System.out.println(count);
         }
-
-        return result;
     }
 
+    static void dfs(int row, int col) {
+        map[row][col] = '.';
+
+        for (int[] direction : DIRECTIONS) {
+            int newRow = row + direction[0];
+            int newCol = col + direction[1];
+
+            if (isValid(newRow, newCol) && map[newRow][newCol] == '#') {
+                dfs(newRow, newCol);
+            }
+        }
+    }
+
+    static boolean isValid(int row, int col) {
+        return row >= 0 && row < rowSize && col >= 0 && col < colSize;
+    }
 }
