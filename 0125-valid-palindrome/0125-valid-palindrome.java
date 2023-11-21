9b9efdf3ad423d8e0231d16fd56a488fb58b2da6
JWbase <87960006+JWbase@users.noreply.github.com>
2023-11-21 20:11:40 +0900

    Time: 19 ms (37.26%), Space: 43.4 MB (50.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-
-        while(start < end) {
-            // 영문자 인지 판단하고 유효하지 않으면 한칸 씩 이동
-            if (!Character.isLetterOrDigit(s.charAt(start))) {
-                start++;
-            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
-                end--;
-            } else {
-                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
-                    return false;
-                }
-                start++;
-                end--;
-            }
-        }
-        return true;
+        String s_filtered = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String s_reversed = new StringBuilder(s_filtered).reverse().toString();
+        return s_filtered.equals(s_reversed);
     }
 }
