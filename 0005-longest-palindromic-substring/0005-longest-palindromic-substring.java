bd71f49848e5bf7d97bea585b4d2dd98f0ae6a6b
JWbase <87960006+JWbase@users.noreply.github.com>
2023-12-02 23:50:11 +0900

    Time: 14 ms (88.43%), Space: 42 MB (63.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,15 +2,14 @@ class Solution {
     int left, maxLen;
     
     public void extendPalindrome(String s, int j, int k) {
-        
         while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
             j--;
             k++;
         }
         
         if (maxLen < k - j - 1) {
-            left = j + 1;
             maxLen = k - j - 1;
+            left = j + 1;
         }
         
     }
