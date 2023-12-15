5e77b44fc77480035d5965f57a7b52a791023f35
JWbase <87960006+JWbase@users.noreply.github.com>
2023-12-16 02:08:03 +0900

    Time: 2 ms (81.41%), Space: 43.8 MB (37.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,11 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        
+        //하나의 for 반복으로 통합
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            // target에서 num을 뺀 값이 있으면 정답으로 리턴
+            if (numsMap.containsKey(target - nums[i])) {
+                return new int[] {i, numsMap.get(target - nums[i])};
             }
+            
+            numsMap.put(nums[i], i);
         }
         return null;
     }
