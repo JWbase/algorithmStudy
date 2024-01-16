d94bfd420652b33d490b87e1985ceed307d117fd
JWbase <87960006+JWbase@users.noreply.github.com>
2024-01-16 22:28:43 +0900

    [Silver II] Title: 시간표 짜기, Time: 272 ms, Memory: 19760 KB -BaekjoonHub

--- 백준/Silver/14569. 시간표 짜기/시간표 짜기.java
+++ 백준/Silver/14569. 시간표 짜기/시간표 짜기.java
@@ -1,42 +1,32 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        boolean[][] subjects = new boolean[N][51];
+        long[] subjects = new long[N];
         for (int i = 0; i < N; i++) {
-            String[] line = br.readLine().split(" ");
-            int k = Integer.parseInt(line[0]);
-            for (int j = 1; j <= k; j++) {
-                int t = Integer.parseInt(line[j]);
-                subjects[i][t] = true;
+            String[] input = br.readLine().split(" ");
+            for (int j = 1; j < input.length; j++) {
+                int t = Integer.parseInt(input[j]) - 1;
+                subjects[i] |= (1L << t);
             }
         }
 
         int M = Integer.parseInt(br.readLine());
         for (int i = 0; i < M; i++) {
-            String[] line = br.readLine().split(" ");
-            int p = Integer.parseInt(line[0]);
-            boolean[] student = new boolean[51];
-            for (int j = 1; j <= p; j++) {
-                int q = Integer.parseInt(line[j]);
-                student[q] = true;
+            String[] input = br.readLine().split(" ");
+            long student = 0;
+            for (int j = 1; j < input.length; j++) {
+                int q = Integer.parseInt(input[j]) - 1;
+                student |= (1L << q);
             }
 
             int count = 0;
-            for (int j = 0; j < N; j++) {
-                boolean canTake = true;
-                for (int k = 1; k <= 50; k++) {
-                    if (subjects[j][k] && !student[k]) {
-                        canTake = false;
-                        break;
-                    }
-                }
-                if (canTake) count++;
+            for (long subject : subjects) {
+                if ((student & subject) == subject) count++;
             }
 
             System.out.println(count);
