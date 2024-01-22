4e4e974b114e1094b718115faffb8b6b9cfbe95d
JWbase <jungwoo1002@gmail.com>
2024-01-22 09:56:48 +0900

    [Silver II] Title: 안녕, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -3,33 +3,32 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] energy = new int[n+1];
-		for(int i=1; i<n+1; i++) {
-			energy[i] = Integer.parseInt(st.nextToken());
-		}
-		st = new StringTokenizer(br.readLine());
-		int[] gift = new int[n+1];
-		for(int i=1; i<n+1; i++) {
-			gift[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] dp = new int[101];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] hp = new int[n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            hp[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int[] happy = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            happy[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[101];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 99; j >= 0; j--) {
+                int energy = hp[i] + j;
+                if (energy < 100) {
+                    dp[energy] = Math.max(dp[energy], dp[j] + happy[i]);
+                }
+            }
+        }
         
-		for(int i=1; i<n+1; i++) {
-			for(int j=99; j>=0; j--) {
-				int ene = energy[i]+j;
-				if(ene<100) {
-					dp[ene] = Math.max(dp[ene], dp[j]+gift[i]);
-				}
-			}
-		}
-		System.out.println(dp[99]);
-		
-	}
+        System.out.println(dp[99]);
+    }
 }
