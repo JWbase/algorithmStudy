de41c35e6221a95b105c81f68369ba8646a3f6fa
JWbase <jungwoo1002@gmail.com>
2024-11-09 13:13:38 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,17 @@
+WITH RankedEcoli AS (
+    SELECT 
+        ID,
+        SIZE_OF_COLONY,
+        NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS quartile
+    FROM ECOLI_DATA
+)
+SELECT 
+    ID,
+    CASE 
+        WHEN quartile = 1 THEN 'CRITICAL'
+        WHEN quartile = 2 THEN 'HIGH'
+        WHEN quartile = 3 THEN 'MEDIUM'
+        WHEN quartile = 4 THEN 'LOW'
+    END AS COLONY_NAME
+FROM RankedEcoli
+ORDER BY ID;
