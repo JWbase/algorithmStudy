f677323c6e9d5b222eef4b143b00d89c2f5b372e
JWbase <jungwoo1002@gmail.com>
2024-12-27 19:41:35 +0900

    [level 2] Title: 방문 길이, Time: 17.80 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,37 +2,35 @@ import java.util.*;
 
 class Solution {
     
-    private static boolean isValid(int nx, int ny) {
-        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
+    private static boolean validate(int nx, int ny) {
+        return nx >= 0 && nx < 11 && ny >= 0 && ny < 11;
     }
     
-    private static final Map<Character, int[]> location = new HashMap<>();
-    
-    private static void init() {
-        location.put('U', new int[]{0, 1});
-        location.put('D', new int[]{0, -1});
-        location.put('L', new int[]{-1, 0});
-        location.put('R', new int[]{1, 0});
+    Map<Character, int[]> directions = new HashMap<>();
+    private void init() {
+        directions.put('U', new int[]{0, 1});
+        directions.put('D', new int[]{0, -1});
+        directions.put('L', new int[]{-1, 0});
+        directions.put('R', new int[]{1, 0});
     }
     
     public int solution(String dirs) {
         init();
         int x = 5, y = 5;
-        Set<String> answer = new HashSet<>();
-        
-        for (int i = 0; i < dirs.length(); i++) {
-            int[] offset = location.get(dirs.charAt(i));
+        Set<String> records = new HashSet<>();
+        for(int i = 0; i < dirs.length(); i++) {
+            int[] offset = directions.get(dirs.charAt(i));
             int nx = x + offset[0];
             int ny = y + offset[1];
-            if (!isValid(nx, ny)) {
+            if (!validate(nx, ny)) {
                 continue;
             }
-            answer.add(x + " " + y + " " + nx + " " + ny);
-            answer.add(nx + " " + ny + " " + x + " " + y);
+            records.add(x + " " + y + " " + nx + " " + ny);
+            records.add(nx + " " + ny + " " + x + " " + y);
             x = nx;
             y = ny;
         }
         
-        return answer.size() / 2;
+        return records.size() / 2;
     }
 }
