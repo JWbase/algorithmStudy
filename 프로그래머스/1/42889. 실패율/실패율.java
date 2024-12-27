a74541eb1c7ee2e53d933c7d8e7dc76755f1a37d
JWbase <jungwoo1002@gmail.com>
2024-12-27 11:44:35 +0900

    [level 1] Title: 실패율, Time: 9.92 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,30 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
+        //스테이지 별 도전자 수
         int[] challenger = new int[N + 2];
-        //스테이지 별 도전자 구하기
         for (int i = 0; i < stages.length; i++) {
             challenger[stages[i]] += 1;
         }
         
-        //스테이지 별 실패한 사용자 수 계산
+        // 스테이지 별 실패한 사용자 수 계산
         Map<Integer, Double> fails = new HashMap<>();
         double total = stages.length;
         
-        //각 스테이지를 순회하여 실패율 계산
         for (int i = 1; i <= N; i++) {
             if (challenger[i] == 0) {
-                fails.put(i, 0.0);
+                fails.put(i, 0.);
             } else {
                 fails.put(i, challenger[i] / total);
                 total -= challenger[i];
             }
         }
         
-        
-        return fails.entrySet()
-            .stream()
-            .sorted((o1,o2) -> Double.compare(o2.getValue(), o1.getValue()))
+        return fails.entrySet().stream()
+            .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
             .mapToInt(HashMap.Entry::getKey)
             .toArray();
     }
