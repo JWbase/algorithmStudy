846c5c3cabd664245ede9bfebd9d7705bee512f6
JWbase <jungwoo1002@gmail.com>
2024-12-27 20:01:31 +0900

    [level 2] Title: 올바른 괄호, Time: 20.28 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,18 +3,19 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
-        
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                stack.push(c);
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
             } else {
-                if (stack.isEmpty() || stack.pop() == ')') {
+                if (stack.isEmpty()) {
                     return false;
                 }
+                stack.pop();
             }
         }
-        
-        return stack.isEmpty() ? true : false;
+        if (stack.isEmpty()) {
+            return true;    
+        }
+        return false;
     }
 }
