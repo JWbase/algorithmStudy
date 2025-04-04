7a6f92ab356d1fcf5cf37af0478d9a8b1524d620
JWbase <jungwoo1002@gmail.com>
2025-04-04 23:46:33 +0900

    [level 3] Title: 등굣길, Time: 0.66 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        final int MOD = 1000000007;
+        int[][] dp = new int[n+1][m+1];
+        
+        boolean[][] isPuddle = new boolean[n+1][m+1];
+        for (int[] puddle : puddles) {
+            int x = puddle[0];
+            int y = puddle[1];
+            isPuddle[y][x] = true;
+        }
+        
+        dp[1][1] = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 && j == 1) continue;
+                
+                if (isPuddle[i][j]) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
